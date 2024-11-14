4e61e0e94eb62685d999a148fe899fd5a8c4856d
Justin ! <just1602@riseup.net>
2024-11-14 01:51:14 -0500

    feat: support `~/` to refer to `$HOME` in config file path

--- src/config.rs
+++ src/config.rs
@@ -1,4 +1,4 @@
-use std::path::PathBuf;
+use std::path::{Path, PathBuf};
 
 use serde::Deserialize;
 
@@ -20,6 +20,47 @@ impl Config {
     }
 
     pub fn todo_file(&self) -> PathBuf {
-        self.todo_dir.join("todo.txt")
+        let base_path = if self.todo_dir.starts_with("~") {
+            let home_path = std::env::var("HOME").expect("Failed to retrieve HOME dir path");
+
+            let folder = self.todo_dir.to_string_lossy();
+            let folder = folder.trim_start_matches("~/");
+
+            Path::new(&home_path).join(&folder)
+        } else {
+            self.todo_dir.clone()
+        };
+
+        base_path.join("todo.txt")
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::Config;
+    use std::path::PathBuf;
+
+    #[test]
+    fn it_returns_the_todo_file_name() {
+        let config = Config {
+            todo_dir: PathBuf::from("/home/test/.todo"),
+        };
+
+        assert!(config.todo_file().ends_with("/home/test/.todo/todo.txt"));
+    }
+
+    #[test]
+    fn it_support_tild_as_home_dir() {
+        #[cfg(target_os = "linux")]
+        let home_path_prefix = "/home/";
+        #[cfg(target_os = "macos")]
+        let home_path_prefix = "/Users/";
+
+        let config = Config {
+            todo_dir: PathBuf::from("~/.todo"),
+        };
+
+        assert!(config.todo_file().starts_with(home_path_prefix));
+        assert!(config.todo_file().ends_with(".todo/todo.txt"));
     }
 }
