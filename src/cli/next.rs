31f9ad52016f3fd632bef5998b9fdfcd2f14578e
Justin ! <just1602@riseup.net>
2024-11-22 00:38:57 -0500

    feat: add query support for the next command

--- src/cli/next.rs
+++ src/cli/next.rs
@@ -2,7 +2,7 @@ use clap::Parser;
 
 use crate::{
     storage::TaskStorage,
-    tasks::{error::TaskError, list::TaskListTrait},
+    tasks::{error::TaskError, list::TaskListTrait, query::TaskQuery},
 };
 
 use super::print_tasks_list;
@@ -12,12 +12,21 @@ use super::print_tasks_list;
     name = "next",
     about = "Show the next task to do base on the urgency task sort we have"
 )]
-pub struct Next;
+pub struct Next {
+    #[arg(trailing_var_arg = true, allow_hyphen_values = true)]
+    query: Option<Vec<String>>,
+}
 
 impl Next {
     pub fn execute(&self, storage: TaskStorage) -> Result<(), TaskError> {
-        let tasks = storage.get_all()?;
+        let mut tasks = storage.get_all()?;
         let total = tasks.len();
+
+        if let Some(query) = &self.query {
+            let query = TaskQuery::from_string_vec(query.clone())?;
+
+            tasks = tasks.filter_from_query(&query).collect();
+        }
 
         if let Some(task) = tasks.sort_by_urgency().first() {
             // FIXME: remove this clone
